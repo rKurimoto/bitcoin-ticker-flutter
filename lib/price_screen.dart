ae0db505c4863ee9f38aac56cc1b5c0cba802079
rKurimoto <rei.kurimoto.invest@gmail.com>
2022-03-26 13:37:10 +0900

    Create lib files

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,57 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  dynamic rate = CoinData().getCoinData();
+
+  // Future<dynamic> getCoinExchangeRate() async {
+  //   var rate = await CoinData().getCoinData();
+  //   return rate;
+  // }
+
+  DropdownButton androidDropdownButton() {
+    List<DropdownMenuItem> menuList = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      menuList.add(
+        DropdownMenuItem(
+          child: Text(currenciesList[i]),
+          value: currenciesList[i],
+        ),
+      );
+    }
+    return DropdownButton(
+      value: selectedCurrency,
+      items: menuList,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(
+        Text(currency),
+      );
+    }
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   rate = getCoinExchangeRate();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +81,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ${rate.toString()} USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +96,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iOSPicker() : androidDropdownButton(),
           ),
         ],
       ),
